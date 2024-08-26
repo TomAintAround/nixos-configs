3715b8d92ece65c872f98e23dc571984dbe8d91f
TomAintAround <tcorreia2005@proton.me>
2024-08-26 16:46:37 +0100

    feat(home-manager): remove gamescope

--- home-manager/modules/gaming.nix
+++ home-manager/modules/gaming.nix
@@ -1,7 +1,6 @@
 { pkgs, lib, config, ... }: {
     home.packages = with pkgs; lib.mkIf config.gaming.enable [
         bottles
-        gamescope
         heroic
         lutris
         prismlauncher
