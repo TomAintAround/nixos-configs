c3c2286562c874f078bdc8be3d358afcb6fdc7ea
TomAintAround <tcorreia2005@proton.me>
2024-08-29 17:35:19 +0100

    fix: flickering lights with openrgb

--- system-config/modules/hardware/openrgb.nix
+++ system-config/modules/hardware/openrgb.nix
@@ -1,3 +1,8 @@
 {
     services.hardware.openrgb.enable = true;
+
+    # This service is already started at startup.
+    # With this service being also enable, there
+    # will be conflict and the lights will flicker
+    systemd.services.openrgb.enable = false;
 }
