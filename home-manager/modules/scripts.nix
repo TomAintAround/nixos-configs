84c08c301a17c278c8657807f460fde5ebab9962
TomAintAround <tcorreia2005@proton.me>
2024-10-22 01:09:12 +0100

    style(home-manager): fix identation

--- home-manager/modules/scripts.nix
+++ home-manager/modules/scripts.nix
@@ -33,42 +33,42 @@
 
             mode() {
                 if [[ $1 =~ ^[0-9]+$ ]] && [[ $(ls $wallpapersDir | grep -ow "$1") ]]; then
-                new="$1"
-                printf "\033[0;32m[MODE]\033[0m Direct\n"
+                    new="$1"
+                    printf "\033[0;32m[MODE]\033[0m Direct\n"
                 elif [[ "$1" = "startup" ]]; then
-                new=$selected
+                    new=$selected
                 else
-                wallpapernum=$(ls $wallpapersDir | wc -l)
-                new="$(($selected + 1))"
-                if [ "$new" -gt "$wallpapernum" ]; then
-                    new=1
-                fi
-                printf "\033[0;32m[MODE]\033[0m Cycle\n"
+                    wallpapernum=$(ls $wallpapersDir | wc -l)
+                    new="$(($selected + 1))"
+                    if [ "$new" -gt "$wallpapernum" ]; then
+                        new=1
+                    fi
+                    printf "\033[0;32m[MODE]\033[0m Cycle\n"
                 fi
             }
 
             wayland() {
                 if [[ $(ps -ef | grep swww-daemon | sed '/grep/d' | cat -s) ]]; then
-                mode "$1"
-                printf "\033[0;32m[INFO]\033[0m Selecting wallpaper $new.jpg\n"
-                ${pkgs.swww}/bin/swww img $wallpapersDir/"$new".jpg --transition-step 150 --transition-type wipe --transition-bezier .33,1,.67,1
+                    mode "$1"
+                    printf "\033[0;32m[INFO]\033[0m Selecting wallpaper $new.jpg\n"
+                    ${pkgs.swww}/bin/swww img $wallpapersDir/"$new".jpg --transition-step 150 --transition-type wipe --transition-bezier .33,1,.67,1
                 else
-                mode "$1"
-                printf "\033[0;32m[INFO]\033[0m Initializing SWWW and selecting wallpaper $new.jpg\n"
-                ${pkgs.swww}/bin/swww init
-                ${pkgs.swww}/bin/swww img $wallpapersDir/"$new".jpg
+                    mode "$1"
+                    printf "\033[0;32m[INFO]\033[0m Initializing SWWW and selecting wallpaper $new.jpg\n"
+                    ${pkgs.swww}/bin/swww init
+                    ${pkgs.swww}/bin/swww img $wallpapersDir/"$new".jpg
                 fi
             }
 
             case "$XDG_SESSION_TYPE" in
                 "wayland")
-                wayland "$1"
+                    wayland "$1"
                 ;;
 
                 *)
-                mode "$1"
-                printf "\033[0;32m[INFO]\033[0m Selecting wallpaper $new.jpg\n"
-                ${pkgs.feh}/bin/feh --bg-fill $wallpapersDir/"$new".jpg
+                    mode "$1"
+                    printf "\033[0;32m[INFO]\033[0m Selecting wallpaper $new.jpg\n"
+                    ${pkgs.feh}/bin/feh --bg-fill $wallpapersDir/"$new".jpg
             esac
 
             echo "$new" > "$XDG_DATA_HOME"/scripts/wallpaper/wallpaper-set
