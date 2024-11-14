74ca7f817a76c496a95ea06e5b20c64cad0bc492
TomAintAround <tcorreia2005@proton.me>
2024-11-14 14:54:27 +0000

    fix(home-manager): fix some aliased commands running twice

--- home-manager/modules/fish/aliases.nix
+++ home-manager/modules/fish/aliases.nix
@@ -72,11 +72,11 @@
         };
 
         shellAliases = {
-            "cat" = "${pkgs.bat}/bin/bat --paging=never $argv";
-            "less" = "${pkgs.bat}/bin/bat --paging=always $argv";
-            "lf" = "cd \"$($XDG_CONFIG_HOME/home-manager/modules/lf/lfimg -print-last-dir $argv)\"";
+            "cat" = "${pkgs.bat}/bin/bat --paging=never";
+            "less" = "${pkgs.bat}/bin/bat --paging=always";
+            "lf" = "cd \"$($XDG_CONFIG_HOME/home-manager/modules/lf/lfimg -print-last-dir)\"";
             "ls" = "${pkgs.eza}/bin/eza --git --icons=always --long --all --group --header --links --color=always --no-quotes --smart-group --group-directories-first --time-style='+%H:%m %d/%m/%y'";
-            "man" = "${pkgs.bat-extras.batman}/bin/batman $argv";
+            "man" = "${pkgs.bat-extras.batman}/bin/batman";
         };
     };
 }
