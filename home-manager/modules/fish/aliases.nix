e2982c005f03a31d97416c4bc137d3f3e318d106
TomAintAround <tcorreia2005@proton.me>
2024-12-16 02:50:08 +0000

    feat(home-manager/fish): add a arduino-cli upload function

--- home-manager/modules/fish/aliases.nix
+++ home-manager/modules/fish/aliases.nix
@@ -6,6 +6,19 @@
                 body = "clear && exec fish";
             };
 
+            arduino-eject = {
+                description = "Compile and upload arduino file into Arduino and open the serial monitor";
+                body = ''
+                    set file $argv[1]
+                    set usb $argv[2]
+                    set fqbn $argv[3]
+
+                    arduino-cli compile --fqbn $fqbn $file
+                    and arduino-cli upload -p $usb --fqbn $fqbn $file
+                    and arduino-cli monitor -p $usb --fqbn $fqbn
+                '';
+            };
+
             flatclean = {
                 description = "Removes orphaned flatpaks";
                 body = ''
