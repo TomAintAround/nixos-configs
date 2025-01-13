2e3495ffa4869ab3cd00ef146b9456fc5bce71ac
TomAintAround <tcorreia2005@proton.me>
2025-01-13 08:25:51 +0000

    chore(system/desktop): disable printing

--- system-config/hosts/desktop/default.nix
+++ system-config/hosts/desktop/default.nix
@@ -14,22 +14,22 @@
 			../../modules/hardware/openrazer.nix
 			../../modules/hardware/openrgb.nix
 			../../modules/hardware/ssd.nix
-			(
-				import ../../modules/hardware/printing.nix {
-					ensurePrinters = [
-						{
-							name = "EPSON_XP-335";
-							deviceUri = "usb://EPSON/XP-332%20335%20Series?serial=573238503130333276&interface=1";
-							model = "epson-inkjet-printer-escpr/Epson-XP-332_335_Series-epson-escpr-en.ppd";
-							ppdOptions = {
-								PageSize = "A4";
-							};
-						}
-					];
-
-					drivers = with pkgs; [ epson-escpr epsonscan2 ];
-				}
-			)
+			# (
+			# 	import ../../modules/hardware/printing.nix {
+			# 		ensurePrinters = [
+			# 			{
+			# 				name = "EPSON_XP-335";
+			# 				deviceUri = "usb://EPSON/XP-332%20335%20Series?serial=573238503130333276&interface=1";
+			# 				model = "epson-inkjet-printer-escpr/Epson-XP-332_335_Series-epson-escpr-en.ppd";
+			# 				ppdOptions = {
+			# 					PageSize = "A4";
+			# 				};
+			# 			}
+			# 		];
+			#
+			# 		drivers = with pkgs; [ epson-escpr epsonscan2 ];
+			# 	}
+			# )
 			../../modules/users/root.nix
 			../../modules/users/tomm.nix
 			../../modules/virt
