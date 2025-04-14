58e96f6232405b5dfd2618800e95ba66d485b017
TomAintAround <tcorreia2005@proton.me>
2025-04-14 22:01:40 +0100

    feat: update neovim config

--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -1,4 +1,39 @@
-{ lib, ... }: {
+{ pkgs, lib, ... }: {
 	programs.neovim.enable = true;
 	xdg.configFile."nvim".source = lib.cleanSource ./neovim;
+
+	home.packages = with pkgs; [
+		# Requirements
+		curl
+		fd
+		gccgo14
+		gnumake
+		man-pages
+		ripgrep
+
+		# LSPs
+		arduino-language-server
+		basedpyright
+		bash-language-server
+		clang-tools
+		fish-lsp
+		lua-language-server
+		marksman
+		nixd
+		vscode-langservers-extracted
+		taplo # TOMvscode-langservers-extractedL
+		yaml-language-server
+
+		# Formatters
+		jq
+		prettierd
+		ruff # Python
+		stylua
+		yamlfmt
+
+		# Debuggers
+		bashdb
+		completely # dependency of bashdb
+		nodejs_23 # required for lua debugger
+	];
 }
