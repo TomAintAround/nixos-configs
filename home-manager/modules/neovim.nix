985a8bda881535f974c3fbe7104e9d3cada31f1e
TomAintAround <tcorreia2005@proton.me>
2025-04-21 17:08:31 +0100

    feat(home-manager/neovim): instead of sourcing, change the default path

--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -1,39 +1,46 @@
-{ pkgs, lib, ... }: {
+{ pkgs, ... }: {
 	programs.neovim.enable = true;
-	xdg.configFile."nvim".source = lib.cleanSource ./neovim;
 
-	home.packages = with pkgs; [
-		# Requirements
-		curl
-		fd
-		gccgo14
-		gnumake
-		man-pages
-		ripgrep
+	home = {
+		packages = with pkgs; [
+			# Requirements
+			curl
+			fd
+			gccgo14
+			gnumake
+			man-pages
+			ripgrep
 
-		# LSPs
-		arduino-language-server
-		basedpyright
-		bash-language-server
-		clang-tools
-		fish-lsp
-		lua-language-server
-		marksman
-		nixd
-		vscode-langservers-extracted
-		taplo # TOMvscode-langservers-extractedL
-		yaml-language-server
+			# LSPs
+			arduino-language-server
+			basedpyright
+			bash-language-server
+			clang-tools
+			fish-lsp
+			lua-language-server
+			marksman
+			nixd
+			vscode-langservers-extracted
+			taplo # TOML
+			yaml-language-server
 
-		# Formatters
-		jq
-		prettierd
-		ruff # Python
-		stylua
-		yamlfmt
+			# Formatters
+			jq
+			prettierd
+			ruff # Python
+			stylua
+			yamlfmt
 
-		# Debuggers
-		bashdb
-		completely # dependency of bashdb
-		nodejs_23 # required for lua debugger
-	];
+			# Debuggers
+			bashdb
+			completely # dependency of bashdb
+			nodejs_23 # required for lua debugger
+		];
+		# I'm absolutely not configuring neovim through nix,
+		# so here's my solution that doesn't rely on sourcing
+		# (which makes the configuration read-only)
+		sessionVariables = {
+			NVIM_APPNAME = "home-manager/modules/neovim";
+		};
+	};
 }
