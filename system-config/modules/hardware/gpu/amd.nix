7881424ef3b4e59c1dc84ebf9b59c2a40f60061b
TomAintAround <tcorreia2005@proton.me>
2025-08-10 07:14:00 +0100

    feat(system/amdgpu): add missing configuration

--- system-config/modules/hardware/gpu/amd.nix
+++ system-config/modules/hardware/gpu/amd.nix
@@ -1,13 +1,21 @@
 {lib, ...}: {
   services.xserver.videoDrivers = lib.mkDefault ["amdgpu" "modesetting"];
 
+  nixpkgs.config.rocmSupport = true;
+
   hardware = {
     amdgpu = {
       opencl.enable = true;
+      initrd.enable = true;
       amdvlk = {
         enable = true;
         support32Bit.enable = true;
       };
     };
+  };
+
+  environment.variables = {
+    LIBVA_DRIVER_NAME = "radeonsi";
+    VDPAU_DRIVER = "radeonsi";
   };
 }
