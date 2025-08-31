be06295f088d478b5463b0ef5c6bde10b12e390a
TomAintAround <tcorreia2005@proton.me>
2025-08-31 21:00:02 +0100

    feat(home/firefox): change arkenfox conf

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -26,53 +26,41 @@
 
     file = {
       ".mozilla/firefox/5zm1o3x5.default-release/user-overrides.js".text = ''
-        /* 0401 */
-        user_pref("browser.safebrowsing.malware.enabled", false);
-        user_pref("browser.safebrowsing.phishing.enabled", false);
-
-        /* 0402 */
-        user_pref("browser.safebrowsing.downloads.enabled", false);
-
-        /* 0710 */
+        // 0710
         user_pref("network.trr.mode", 2);
 
-        /* 0801 */
-        user_pref("keyword.enabled", true);
+        // 0712
+        user_pref("network.trr.uri", "https://dns.quad9.net/dns-query");
+        user_pref("network.trr.custom_uri", "https://dns.quad9.net/dns-query")
 
-        /* 0830 */
+        // 0830
         user_pref("browser.search.separatePrivateDefault", false);
         user_pref("browser.search.separatePrivateDefault.ui.enabled", false);
 
-        /* 1006 */
+        // 1006
         user_pref("browser.shell.shortcutFavicons", true);
 
-        /* 5003 */
+        // 5003
         user_pref("signon.rememberSignons", false);
 
-        /* 5009 */
-        user_pref("network.dns.disableIPv6", true);
-
-        /* 5017 */
+        // 5017
         user_pref("extensions.formautofill.addresses.enabled", false);
         user_pref("extensions.formautofill.creditCards.enabled", false);
 
-        /* Disable pocket */
-        user_pref("extensions.pocket.enabled", false);
-
-        /* Disable Mozilla account */
+        // Disable Mozilla account
         user_pref("identity.fxaccounts.enabled", false);
 
-        /* Block autoplay */
+        // Block autoplay
         user_pref("media.autoplay.default", 5);
 
-        /* Enabling userChrome customization */
+        // Enabling userChrome customization
         user_pref("toolkit.legacyuserprofilecustomizations.stylesheets", true);
 
-        /* Enable Hardware Acceleration */
+        // Enable Hardware Acceleration
         user_pref("gfx.webrender.all", true);
         user_pref("media.ffmpeg.vaapi.enabled", true);
 
-        /* Make firefox boot faster */
+        // Make firefox boot faster
         user_pref("widget.use-xdg-desktop-portal.settings", 0);
       '';
     };
