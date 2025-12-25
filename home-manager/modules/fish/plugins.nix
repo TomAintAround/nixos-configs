9d7f24a820f703a1457efa66359aa73b725dcd0d
TomAintAround <tcorreia2005@proton.me>
2025-12-25 02:07:36 +0000

    feat(home/fish): remove pufferfish plugin

--- home-manager/modules/fish/plugins.nix
+++ home-manager/modules/fish/plugins.nix
@@ -9,10 +9,6 @@
       src = pkgs.fishPlugins.autopair.src;
     }
     {
-      name = "pufferfish";
-      src = pkgs.fishPlugins.puffer.src;
-    }
-    {
       name = "tide";
       src = pkgs.fishPlugins.tide.src;
     }
