6f92d40761e921705a154f0ffc1ea6dbd03ce2d3
TomAintAround <tcorreia2005@proton.me>
2026-01-26 06:58:27 +0000

    fix(home/flameshot): fix fractional scaling bug

--- home-manager/modules/themes/default.nix
+++ home-manager/modules/themes/default.nix
@@ -28,23 +28,36 @@
       run ln -s /run/current-system/sw/share/X11/fonts ${config.xdg.dataHome}/fonts 2>/dev/null
     '';
 
-    sessionVariables = {
+    sessionVariables = let
+      monitor = builtins.elemAt (builtins.filter (m: m.default) config.monitors) 0;
+      inherit (monitor) scale;
+    in {
       GTK_THEME = config.gtk.theme.name;
       ICON_THEME = config.gtk.iconTheme.name;
       HYPRCURSOR_THEME = config.home.pointerCursor.name;
       HYPRCURSOR_SIZE = 20;
       KRITA_NO_STYLE_OVERRIDE = 1;
-      GDK_SCALE = let
-        monitor = builtins.elemAt (builtins.filter (m: m.default) config.monitors) 0;
-      in
-        monitor.scale;
+      GDK_SCALE = scale;
+      QT_AUTO_SCREEN_SCALE_FACTOR = toString (1.0 / scale);
     };
   };
 
   qt = {
     enable = true;
-    platformTheme.name = "kvantum";
+    platformTheme.name = "qtct";
     style.name = "kvantum";
+    qt6ctSettings = {
+      Appearance = {
+        style = "kvantum";
+        icon_theme = config.gtk.iconTheme.name;
+        standar_dialogs = "xdgdesktopportal";
+      };
+      Fonts = {
+        general = "\"${config.gtk.font.name},${toString (builtins.floor config.gtk.font.size)}\"";
+        fixed = "\"${config.gtk.font.name},${toString (builtins.floor config.gtk.font.size)}\"";
+      };
+    };
+    qt5ctSettings = config.qt.qt6ctSettings;
   };
 
   wayland.windowManager.hyprland.settings.plugin.hyprfocus.bar_text_font = config.gtk.font.name;
