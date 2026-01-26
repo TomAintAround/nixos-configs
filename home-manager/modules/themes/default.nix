f2b21fff45a4938e0a43a1bf618b999764624cb9
TomAintAround <tcorreia2005@proton.me>
2026-01-26 06:57:44 +0000

    style(home/themes): remove empty lines

--- home-manager/modules/themes/default.nix
+++ home-manager/modules/themes/default.nix
@@ -8,13 +8,11 @@
 
   gtk = {
     enable = true;
-
     font = {
       name = "Inter";
       size = 11.0;
       package = pkgs.inter;
     };
-
     gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
   };
 
