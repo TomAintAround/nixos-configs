f6827f9105ebc73a24fc5a8d699cecd1d888f842
TomAintAround <tcorreia2005@proton.me>
2026-01-28 08:36:31 +0000

    feat(home/hypr): dim inactive apps

--- home-manager/modules/des-wms/hyprland/general.nix
+++ home-manager/modules/des-wms/hyprland/general.nix
@@ -33,7 +33,7 @@
     decoration = {
       rounding = 15;
       rounding_power = 4.0;
-      dim_inactive = false;
+      dim_inactive = true;
       dim_strength = 0.1;
       blur = {
         size = 7;
