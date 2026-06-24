b18539647f03725bcd91dca9b6f6344b69998042
TomAintAround <tcorreia2005@proton.me>
2026-06-24 17:33:20 +0100

    feat(home): change desktop monitors

--- home-manager/tomm/hosts/desktop.nix
+++ home-manager/tomm/hosts/desktop.nix
@@ -25,7 +25,7 @@
       x = 0;
       y = 0;
       scale = 1.0;
-      enable = true;
+      enable = false;
       default = false;
     }
     {
@@ -37,7 +37,7 @@
       x = 1920;
       y = 0;
       scale = 1.0;
-      enable = false;
+      enable = true;
       default = true;
     }
     {
