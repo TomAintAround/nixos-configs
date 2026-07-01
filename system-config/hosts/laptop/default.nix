a870c443e98bbb4160bab8e66768ab388c81adb0
TomAintAround <tcorreia2005@proton.me>
2026-07-01 01:17:28 +0100

    feat(system): change tlp config and make it laptop only

--- system-config/hosts/laptop/default.nix
+++ system-config/hosts/laptop/default.nix
@@ -45,4 +45,48 @@
 
   networking.hostName = "laptop";
   system.stateVersion = "25.11";
+  services.tlp.settings = {
+    TLP_AUTO_SWITCH = 2;
+
+    TLP_PROFILE_BAT = "SAV";
+
+    # TODO: Remove in 1.10
+    TLP_DEFAULT_MODE = "BAL";
+
+    # TODO: Switch to TLP_PROFILE_DEFAULT in 1.10
+    TLP_PERSISTENT_DEFAULT = 0;
+
+    # My battery only allows for charge control
+    STOP_CHARGE_THRESH_BAT0 = 1;
+
+    # TODO: Remove in 1.10
+    RADEON_DPM_STATE_ON_AC = "performance";
+    RADEON_DPM_STATE_ON_BAT = "balanced";
+
+    CPU_DRIVER_OPMODE_ON_AC = "active";
+    CPU_DRIVER_OPMODE_ON_BAT = "active";
+    CPU_DRIVER_OPMODE_ON_SAV = "active";
+
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    CPU_SCALING_GOVERNOR_ON_SAV = "powersave";
+
+    CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+    CPU_ENERGY_PERF_POLICY_ON_SAV = "power";
+
+    CPU_BOOST_ON_AC = 1;
+    CPU_BOOST_ON_BAT = 0;
+    CPU_BOOST_ON_SAV = 0;
+
+    DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth";
+
+    DEVICES_TO_ENABLE_ON_STARTUP = "wifi";
+
+    DEVICES_TO_DISABLE_ON_LAN_CONNECT = "wifi wwan";
+    DEVICES_TO_DISABLE_ON_WIFI_CONNECT = "wwan";
+    DEVICES_TO_DISABLE_ON_WWAN_CONNECT = "wifi";
+
+    DEVICES_TO_ENABLE_ON_LAN_DISCONNECT = "wifi wwan";
+  };
 }
